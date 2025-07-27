f2b759f256bd42fccc8a3921447a5149ee3c47f2
nairit132 <nairithmajmundar@gmail.com>
2025-07-27 18:55:45 +0100

    Add files via upload

---
+++ task three.py
@@ -0,0 +1,9 @@
+L = [4, 5, 1, 2, 9, 7 ,10 ,8 ]
+print("The original list : ",L)
+count = 0
+for i in L:
+    count += i
+avg = count/len(L)
+print("sum = ", count)
+print("average = ",avg)
+L.sort()
